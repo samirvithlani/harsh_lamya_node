6ce2f89e5eebc4dac2e99e780cbfaaa8133dd681
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-12 16:39:37 +0530

    filename

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,38 +1,62 @@
 const multer = require("multer");
 const uploadFileinCloudnary = require("../utils/CloudnaryUtil");
+const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: "./uploads",
+  //destination: "./uploads",
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
 });
 
+const fileFilter1 = (req, file, cb) => {
+  //console.log("file....",req)
+  const allowedTypes = /jpeg|png|jpg|gif/;
+  console.log(file.mimetype);
+  const extName = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimeType = allowedTypes.test(file.mimetype);
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    return cb(new Error("only images are allowed"), false);
+  }
+};
+
 const upload = multer({
   storage: storage,
-  //fileFilter:
+  fileFilter: fileFilter1,
 }).single("profilepic");
 
 const uploadFile = async (req, res) => {
-  upload(req, res, async (err) => {
-    if (err) {
-      res.status(500).json({
-        message: "error while uploading file..",
-      });
-    } else {
-      //file upload done...
-      //db logic
-      //cloud...
+  
+  
+    upload(req, res, async (err) => {
+      if (err) {
+        ///console.log(err.message);
+        res.status(500).json({
+          message: "error while uploading file..",
+          data:err.message
+        });
+      } else {
+        //file upload done...
+        //db logic
+        //cloud...
 
-      const cloudnaryResponse = await uploadFileinCloudnary(req.file.path);
-      res.status(201).json({
-        message:
-          "file " + req.file.originalname + " is uploaded successfully..",
-        data: req.file,
-        cloundanryresponse: cloudnaryResponse,
-      });
-    }
-  });
+        //const cloudnaryResponse = await uploadFileinCloudnary(req.file.path);
+        //console.log(req.file)
+        console.log(req.body)
+        res.status(201).json({
+          message:
+            "file " + req.file.originalname + " is uploaded successfully..",
+          data: req.file,
+          // cloundanryresponse: cloudnaryResponse,
+        });
+      }
+    });
+  
+  
 };
 
 module.exports = {
